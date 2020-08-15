687fe22d5d8454924749f5a22ee0b5fa75aa640d
Jan Sommer <jan-sommer@outlook.com>
2020-08-15 12:56:43 +0200

    Refactor custom component class by moving template and style definition into class

--- custom.component.js
+++ custom.component.js
@@ -1,27 +1,37 @@
-const style = /*html*/ `
-  <style>
+class CustomComponent extends HTMLElement {
+  _style = document.createElement('style');
+  _template = document.createElement('template');
+
+  constructor() {
+    super();
+
+    this._defineStyle();
+    this._defineTemplate();
+    this._attachToShadowDOM();
+  }
+
+  _defineStyle() {
+    this._style.innerHTML = `
     h1 {
       font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
         Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
         'Segoe UI Symbol';
     }
-  </style>
-`;
+    `;
+  }
 
-const template = document.createElement('template');
-template.innerHTML = /*template*/ `
-  ${style}
+  _defineTemplate() {
+    this._template.innerHTML = `
   <h1>
     Custom Web Component
   </h1>
 `;
+  }
 
-class CustomComponent extends HTMLElement {
-  constructor() {
-    super();
-
+  _attachToShadowDOM() {
     this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.appendChild(this._style);
+    this.shadowRoot.appendChild(this._template.content.cloneNode(true));
   }
 }
 
